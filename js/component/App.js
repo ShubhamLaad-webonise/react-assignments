ebe5e11babe06e35590fd1ac8879c78f5ced421f
ShubhamLaad-webonise <shubhamlaad666@gmail.com>
2016-11-27 12:12:09 +0530

    React setup with basic page

--- js/component/App.js
+++ js/component/App.js
@@ -1,52 +1,15 @@
 import React from 'react';
 import Header from './header';
-import RedBlock from './redBlock';
-import BlueBlock from './BlueBlock';
 
 const App = React.createClass({
-  getDefaultProps () {
-    // console.log("getDefaultProps");
-    return ({
-      appProp: true
-    });
-  },
-
-  getInitialState () {
-    // console.log("getInitialState");
-    return ({
-      appState: true
-    });
-  },
-
-  componentWillMount () {
-    //console.log("componentWillMount");
-    this.setState({
-      appState: this.props.appProp
-    })
-  },
-
-  componentDidMount () {
-    // console.log("did mount");
-  },
-  changeBlock () {
-    this.setState({
-      appState: !this.state.appState
-    })
-  },
-
    render () {
      console.log (this);
       return (
         <div>
           <Header />
-          <h1 className="text-center" onClick={this.changeBlock} ref="ha">
+          <h1 className="text-center" >
             Hello Shubham Laad!
           </h1>
-          <div className="container">
-            {
-              (this.state.appState) ? <RedBlock /> : <BlueBlock />
-            }
-          </div>
         </div>
       )
    }
