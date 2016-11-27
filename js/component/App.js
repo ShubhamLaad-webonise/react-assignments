0dd69741fc7fed84ef94ac07244a2a9ae578fa65
ShubhamLaad-webonise <shubhamlaad666@gmail.com>
2016-11-27 12:27:18 +0530

    Show current time date

--- js/component/App.js
+++ js/component/App.js
@@ -3,13 +3,15 @@ import Header from './header';
 
 const App = React.createClass({
    render () {
-     console.log (this);
+     console.log (this)
+     const now = new Date();
       return (
         <div>
           <Header />
           <h1 className="text-center" >
             Hello Shubham Laad!
           </h1>
+          <p>Todays time: {now.toTimeString()}</p>
         </div>
       )
    }
