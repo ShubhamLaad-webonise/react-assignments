620668979a0a1fa4f689e93180db8428f987b9f1
ShubhamLaad-webonise <shubhamlaad666@gmail.com>
2017-04-23 02:58:06 +0530

    Remove bootstap requier from js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App.js';
-require('bootstrap');
 require('../css/sass.sass');
 
 ReactDOM.render(<App />, document.getElementById('parent'));
